ec92a1b29cdaf08abe25fd9adbb91fadb1525fb0
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-04-28 13:33:57 +0100

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
-""""""
+"""duck-typed annotations"""
 
+
+from typing import Sequence, Any, Union, Mapping, TypeVar
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar, None] = None) -> Union[Any, TypeVar]:
+    """duck-typed annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
