7b20a76565c48d117a993bfde7c25dcc7569179d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-22 20:52:14 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,22 @@
 """unit test"""
 
 
+import unittest
+from unittest.mock import patch
+from client import  GithubOrgClient
+
+
 class TestGithubOrgClient(unittest.TestCase):
     """"test github client"""
-    def test_org(self):
+     @parameterized.expand([
+        ('google'),
+         ('abc')
+        ])
+    @patch('org')
+    def test_org(self, url):
+        """test org"""
+        g =  GithubOrgClient(url)
+        self.assertEqual()
+        
+            
       
