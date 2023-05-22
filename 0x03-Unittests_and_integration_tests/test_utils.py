3712724100a80027f01f2a572188610c7e662303
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-22 15:36:42 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 
 import unittest
 from parameterized import parameterized, parameterized_class
-from unittest import mock
+from unittest.mock import patch
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
 
@@ -26,3 +26,16 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, n, path):
         with self.assertRaises(KeyError) as context:
             access_nested_map(n, path)
+
+class TestGetJson(unittest.TestCase):
+    """test get json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self, url, load):
+        patcher = patch('get_json')
+        mock_read = patcher.start()
+        result = get_json('url')
+        self.assertEqual(result, load)
+        patcher.stop()
