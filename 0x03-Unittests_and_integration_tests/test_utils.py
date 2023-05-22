353e11c33ea3acafaf4a476cb72ad84168fa49f3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-22 15:49:22 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,7 +36,7 @@ class TestGetJson(unittest.TestCase):
     def test_get_json(self, url, load):
         patcher = patch('requests.get', {'return_value.json.return_value': load})
         mock_read = patcher.start()
-        result = get_json('url')
+        result = get_json(url)
         self.assertEqual(result, load)
         mock.assert_called_once()
         patcher.stop()
