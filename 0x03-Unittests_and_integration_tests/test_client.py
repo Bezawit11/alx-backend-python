6c97ac6a50c2b11674bf3513cc497c3971d84e25
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-23 09:23:25 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,6 +5,7 @@
 import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
+get_json = __import__('utils').get_json
 from parameterized import parameterized, parameterized_class
 
 
@@ -14,7 +15,11 @@ class TestGithubOrgClient(unittest.TestCase):
         ('google'),
         ('abc')
         ])
-    @patch('org')
-    def test_org(self, url):
+    @patch('get_json')
+    def test_org(self, url, mock_org):
         """test org"""
+        mock_org.return_value = {}
         g = GithubOrgClient(url)
+        self.assertEqual(g.org(), {})
+        
+        
