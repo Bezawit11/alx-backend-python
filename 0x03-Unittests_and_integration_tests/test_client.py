30d7aaca965c945a2051961ddadfdbd6c5ea1095
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-23 10:19:31 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,8 +5,9 @@
 import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
-get_json = __import__('utils').get_json
 from parameterized import parameterized
+get_json = __import__('utils').get_json
+
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -31,17 +32,19 @@ class TestGithubOrgClient(unittest.TestCase):
             mock.return_value = {"repos_url": "tester"}
             self.assertEqual(c._public_repos_url(), mock.return_value)
 
-    @patch('get_json') 
+    @patch('get_json')
     def test_public_repos(self):
         """test public repos"""
         with patch('GithubOrgClient._public_repos_url') as mock:
             c = GithhubOrgClient('url')
             mock.return_value = [{"tester1": "result1", "tester2": "result2"}]
             self.assertEqual(c._public_repos(), mock.return_value)
+
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
         ])
     def test_has_license(self, repo, license_key, expected):
         """test has license"""
-        self.assertEqual(GithhubOrgClient.has_license(reo, license_key), expected)
+        r = GithhubOrgClient.has_license(repo, license_key)
+        self.assertEqual(r, expected)
